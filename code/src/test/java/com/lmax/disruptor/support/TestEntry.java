51c8a5ce0255408cfd3e723811b88724177474cd
aaron <aaron.pritzlaff@bjss.com>
2011-07-20 15:03:08 +0100

    ignored test helpers for JUnit4 to play nice

--- code/src/test/java/com/lmax/disruptor/support/TestEntry.java
+++ code/src/test/java/com/lmax/disruptor/support/TestEntry.java
@@ -15,23 +15,27 @@
  */
 package com.lmax.disruptor.support;
 
+import org.junit.Ignore;
+
 import com.lmax.disruptor.AbstractEntry;
 import com.lmax.disruptor.EntryFactory;
 
+@Ignore
 public final class TestEntry
-    extends AbstractEntry
+		extends AbstractEntry
 {
-    @Override
-    public String toString()
-    {
-        return "Test Entry";
-    }
+	@Override
+	public String toString()
+	{
+		return "Test Entry";
+	}
 
-    public final static EntryFactory<TestEntry> ENTRY_FACTORY = new EntryFactory<TestEntry>()
-    {
-        public TestEntry create()
-        {
-            return new TestEntry();
-        }
-    };
+	public final static EntryFactory<TestEntry>	ENTRY_FACTORY	= new EntryFactory<TestEntry>()
+																{
+																	@Override
+																	public TestEntry create()
+																	{
+																		return new TestEntry();
+																	}
+																};
 }
--- code/src/test/java/com/lmax/disruptor/support/TestWaiter.java
+++ code/src/test/java/com/lmax/disruptor/support/TestWaiter.java
@@ -15,43 +15,46 @@
  */
 package com.lmax.disruptor.support;
 
-import com.lmax.disruptor.ConsumerBarrier;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CyclicBarrier;
 
+import org.junit.Ignore;
+
+import com.lmax.disruptor.ConsumerBarrier;
+
+@Ignore
 public final class TestWaiter implements Callable<List<StubEntry>>
 {
-    private final long toWaitForSequence;
-    private final long initialSequence;
-    private final CyclicBarrier cyclicBarrier;
-    private final ConsumerBarrier<StubEntry> consumerBarrier;
+	private final long							toWaitForSequence;
+	private final long							initialSequence;
+	private final CyclicBarrier					cyclicBarrier;
+	private final ConsumerBarrier<StubEntry>	consumerBarrier;
 
-    public TestWaiter(final CyclicBarrier cyclicBarrier,
-                      final ConsumerBarrier<StubEntry> consumerBarrier,
-                      final long initialSequence,
-                      final long toWaitForSequence)
-    {
-        this.cyclicBarrier = cyclicBarrier;
-        this.initialSequence = initialSequence;
-        this.toWaitForSequence = toWaitForSequence;
-        this.consumerBarrier = consumerBarrier;
-    }
+	public TestWaiter(final CyclicBarrier cyclicBarrier,
+			final ConsumerBarrier<StubEntry> consumerBarrier,
+			final long initialSequence,
+			final long toWaitForSequence)
+	{
+		this.cyclicBarrier = cyclicBarrier;
+		this.initialSequence = initialSequence;
+		this.toWaitForSequence = toWaitForSequence;
+		this.consumerBarrier = consumerBarrier;
+	}
 
-    @Override
-    public List<StubEntry> call() throws Exception
-    {
-        cyclicBarrier.await();
-        consumerBarrier.waitFor(toWaitForSequence);
+	@Override
+	public List<StubEntry> call() throws Exception
+	{
+		this.cyclicBarrier.await();
+		this.consumerBarrier.waitFor(this.toWaitForSequence);
 
-        final List<StubEntry> messages = new ArrayList<StubEntry>();
-        for (long l = initialSequence; l <= toWaitForSequence; l++)
-        {
-            messages.add(consumerBarrier.getEntry(l));
-        }
+		final List<StubEntry> messages = new ArrayList<StubEntry>();
+		for (long l = this.initialSequence; l <= this.toWaitForSequence; l++)
+		{
+			messages.add(this.consumerBarrier.getEntry(l));
+		}
 
-        return messages;
-    }
+		return messages;
+	}
 }
